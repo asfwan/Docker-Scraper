559d970523be7a69e9b308fb1a9895fb153121ea
saufwan <asf@Ahmads-MacBook-Pro.local>
2021-04-24 17:01:17 +0800

    added label

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker build -t MY_SCRAPER . && docker run -v /dev/shm:/dev/shm --shm-size=2gb -d -p 80:8080 MY_SCRAPER
+#!/bin/bash
+docker build -t my_scraper . && docker run -v /dev/shm:/dev/shm --shm-size=2gb -d -p 80:8080 my_scraper
